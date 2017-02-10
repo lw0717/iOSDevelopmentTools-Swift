fda2c51fb4e3c2efba268eafac8ef0cc4cb284b9
lw0717 <abc414181886@126.com>
2017-02-10 15:33:36 +0800

    添加二维数组LWArray2D

---
+++ Sources/Components/LWArray2D.swift
@@ -0,0 +1,38 @@
+//
+//  LWArray2D.swift
+//  LWSudoku
+//
+//  Created by lw0717 on 17/2/10.
+//  Copyright © 2017年 lw0717. All rights reserved.
+//
+
+import Foundation
+
+struct Array2D<T> {
+    let rows: Int
+    let columns: Int
+    
+    private var array: Array<T?>
+    
+    init(rows: Int, columns: Int) {
+        self.rows = rows
+        self.columns = columns
+        array = Array<T?>(repeating: nil, count: rows * columns)
+    }
+    
+    func indexIsValidForRow(row: Int, column: Int) -> Bool {
+        return row >= 0 && row <= rows && column >= 0 && column <= columns
+    }
+    
+    subscript(row: Int, column: Int) -> T? {
+        get {
+            assert(indexIsValidForRow(row: row, column: column), "Index out of range")
+            return array[(row * columns) + column]
+        }
+        set(newValue) {
+            assert(indexIsValidForRow(row: row, column: column), "Index out of range")
+            array[(row * columns) + column] = newValue
+        }
+    }
+    
+}
