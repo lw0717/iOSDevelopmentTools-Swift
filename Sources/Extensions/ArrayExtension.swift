9890f48a967ed70b51f2369eddd641ee758c65fd
lw0717 <abc414181886@126.com>
2017-03-01 10:20:28 +0800

    添加数组的扩展用于判断是否包含对象，求对象在数组中位置，删除对象

---
+++ Sources/Extensions/ArrayExtension.swift
@@ -0,0 +1,27 @@
+//
+//  ArrayExtension.swift
+//  iOSDevelopmentTools
+//
+//  Created by lw0717 on 17/3/1.
+//  Copyright © 2017年 lw0717. All rights reserved.
+//
+
+import Foundation
+
+extension Array {
+    public func contains<T: Equatable>(_ obj: T) -> Bool {
+        return self.contains(where: { $0 as? T == obj })
+    }
+    
+    public func index<T: Equatable>(_ obj: T) -> Index? {
+        return self.index(where: { $0 as? T == obj })
+    }
+    
+    public mutating func remove<T: Equatable>(_ obj: T) -> Bool {
+        if let index = index(obj) {
+            _ = self.remove(at: index)
+            return true
+        }
+        return false
+    }
+}
